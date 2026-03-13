48f7581f82156bec195be5f974502e7d058fdfb0
Tomas Gomez <tomasgomezzi36gmail.com>
2026-03-13 18:44:15 -0300

    feat: Crea estructura inicial e instala dependencias basicas

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a mi Portfolio</h1>
+      <p>Esta es la página principal.</p>
+    </div>
+  )
+}
+
+export default Home
