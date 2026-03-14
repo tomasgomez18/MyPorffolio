f511d03a064ba2e6ec8befe5e2a3f1e7868b4b22
Tomas Gomez <tomasgomezzi36gmail.com>
2026-03-14 20:41:31 -0300

    fix: Corrige errores de importacion del navbar en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home"
-import { NavBar } from './components/NavBar';
+import NavBar from './components/navbar/NavBar';
+
 
 function App() {
   return (
